b09e7ad83118cda4649f8a6940167fd859af901f
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-04-01 07:30:26 +0000

    sdk

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './components/navbar/Example';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Example />
+    </>
   )
 }
 

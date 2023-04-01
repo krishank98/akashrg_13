de8831bbfce04d9a451a287cd333e125abf87fbe
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-04-01 09:53:45 +0000

    add

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Example from './components/navbar/Example';
+import Footer from './components/footer/Footer';
+import Whatsapp from './components/whatsapp/Whatsapp';
+import Topbar from './components/topbar/Topbar';
 
 
 function App() {
   return (
     <>
+    <Topbar></Topbar>
     <Example ></Example>
+    <div style={{height: '100vh'}}>
+    </div>
+    <Whatsapp></Whatsapp>
+    <Footer></Footer>
 
     </>
 
---
+++ my-project/src/components/whatsapp/Whatsapp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Whatsapp = () => {
+  return (
+    <a
+        href="https://wa.me/8398920183"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
+  )
+}
+
+export default Whatsapp

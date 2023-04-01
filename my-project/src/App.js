e9bb03b031193b2753cd9bb4f0e7f9e50061555d
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-04-01 09:28:23 +0000

    sdasasd

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Example from './components/navbar/Example';
-import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <>
     <Example ></Example>
-    <Footer></Footer>
+
     </>
 
   )

bd73a0f33cb6e9dca205dad9fd67ed0371fe0f9b
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-04-01 12:08:26 +0000

    sad

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -4,6 +4,7 @@ import Example from './components/navbar/Example';
 import Footer from './components/footer/Footer';
 import Whatsapp from './components/whatsapp/Whatsapp';
 import Topbar from './components/topbar/Topbar';
+import Dropdown from './components/dropdown/Dropdown';
 
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
     <>
     <Topbar></Topbar>
     <Example ></Example>
+    
+    <Dropdown style={{margin: "40px" }}></Dropdown>
+    
     <div style={{height: '100vh'}}>
     </div>
     <Whatsapp></Whatsapp>

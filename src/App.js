a8d359f429617e622472460ca6315e5637a4c86f
Krishan Kumar <krishankumarr98@gmail.com>
2023-04-01 18:08:18 +0530

    Dfs

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Example from './components/navbar/Example';
+import Footer from './components/footer/Footer';
+import Whatsapp from './components/whatsapp/Whatsapp';
+import Topbar from './components/topbar/Topbar';
+import Dropdown from './components/dropdown/Dropdown';
+
+
+function App() {
+  return (
+    <>
+    <Topbar></Topbar>
+    <Example ></Example>
+    
+    <Dropdown style={{margin: "40px" }}></Dropdown>
+    
+    <div style={{height: '100vh'}}>
+    </div>
+    <Whatsapp></Whatsapp>
+    <Footer></Footer>
+
+    </>
+
+  )
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div className="bg-blue-800 text-green-300 py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2023 My Company. All rights reserved.
+          Designed by Krishankumarr98@gmail.com
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Topbar

12de191086af19c3c41e0441221516f9309e87f0
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-04-02 14:54:30 +0000

    fsdf

--- src/components/dropdown/DropdownComponent.js
+++ src/components/dropdown/DropdownComponent.js
@@ -4,11 +4,19 @@ export default function DropdownComponent() {
     return (
         <div className="flex items-center justify-center" >
             <select className="p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-                <option>ReactJS Dropdown</option>
-                <option>Laravel 9 with React</option>
-                <option>React with Tailwind CSS</option>
-                <option>React With Headless UI</option>
+                <option>Iphone</option>
+                <option>Iphone 11</option>
+                <option>Iphone 12</option>
+                <option>Iphone 13</option>
             </select>
+            <div>
+            <select className=" p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                <option>Pixel 7</option>
+                <option>Pixel 6a</option>
+                <option>Pixel 4a</option>
+                <option>Pixel 4a 5g</option>
+            </select>
+            </div>
             <div>
             <select className=" p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
                 <option>ReactJS Dropdown</option>
@@ -18,14 +26,6 @@ export default function DropdownComponent() {
             </select>
             </div>
             <div>
-            <select className=" p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-                <option>ReactJS Dropdown</option>
-                <option>Laravel 9 with React</option>
-                <option>React with Tailwind CSS</option>
-                <option>React With Headless UI</option>
-            </select>
-            </div>
-            <div>
             <select className="p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
                 <option>ReactJS Dropdown</option>
                 <option>Laravel 9 with React</option>
@@ -69,7 +69,7 @@ export default function DropdownComponent() {
             </select>
             </div>
 
-            
+
             
         </div>
         

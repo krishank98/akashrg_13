0ae3bc295b29b70d6bd933604aaef2ba5f65a29c
krishankumar <krishankumarr98@gmail.com>
2023-04-02 07:34:42 +0530

    addf

---
+++ mcovers/src/main/java/com/akashrg_13/mcovers/McoversApplication.java
@@ -0,0 +1,13 @@
+package com.akashrg_13.mcovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McoversApplication.class, args);
+	}
+
+}
---
+++ mcovers/src/test/java/com/akashrg_13/mcovers/McoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashrg_13.mcovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

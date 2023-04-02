9b792cfc872fb14c84793a9c61d0195cd52168ba
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-04-02 15:05:55 +0000

    dsfdssas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Example from './components/navbar/Example';
 import Footer from './components/footer/Footer';
 import Whatsapp from './components/whatsapp/Whatsapp';
 import Topbar from './components/topbar/Topbar';
+import Iphone from './components/Iphone/Iphone';
 import DropdownComponent from './components/dropdown/DropdownComponent';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import Dropdown from './components/dropdown/Dropdown';
 
 
@@ -14,7 +16,15 @@ function App() {
     <Topbar></Topbar>
     <Example ></Example>
     
-    <DropdownComponent></DropdownComponent>
+    {/* <DropdownComponent></DropdownComponent> */}
+    <BrowserRouter>
+    <Switch>
+          <Route path="/">
+          <Iphone></Iphone>
+          </Route>
+        </Switch>
+    
+      </BrowserRouter>
     
     
     {/* <Dropdown style={{margin: "40px" }}></Dropdown> */}
---
+++ src/components/Iphone/Iphone.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Iphone = () => {
+  return (
+    <div className="relative w-full lg:max-w-sm" >
+            <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                <option>Iphone</option>
+                <option>Iphone 11</option>
+                <option>Iphone 12</option>
+                <option>Iphone 13</option>
+            </select>
+            </div>
+  )
+}
+
+export default Iphone
